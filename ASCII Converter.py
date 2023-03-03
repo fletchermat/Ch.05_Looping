520490e6d9308084b33f0ea9679502b689b1ca71
fletchermat <fletchermat@urbandaleschools.com>
2023-03-03 08:04:14 -0600

    unit 11

---
+++ ASCII Converter.py
@@ -0,0 +1,37 @@
+# decrypt = [84, 72, 69, 83, 69, 67, 82, 69, 84, 72, 65, 84, 67, 79, 68, 69, 73, 83, 66, 79, 84, 84, 76, 69]
+# decrypted = ""
+# for num in decrypt:
+#     i = chr(num)
+#     decrypted += i
+# print(decrypted)
+
+'''
+
+'''
+A = "F"
+B = 'G'
+C = 'H'
+D = 'I'
+E = 'J'
+F = 'K'
+G = 'L'
+H = 'M'
+I = 'N'
+J = 'O'
+K = 'P'
+L = 'Q'
+M = 'R'
+N = 'S'
+O = 'T'
+P = 'U'
+Q = 'V'
+R = 'W'
+S = 'X'
+T = 'Y'
+U = 'Z'
+V = 'A'
+W = 'B'
+X = 'C'
+Y = 'D'
+Z = 'E'
+print(T,S,E,B,E,H,T,S,K,O,O,L,T,I,D,E,H,G,U,Y,X,E,L,C,I,H,E,V,T,E,G,R,A,T,E,W,D,L,U,O,H,S)
